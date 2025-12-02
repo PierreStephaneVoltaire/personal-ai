aeed2732e80d2ad661391e2fe2546aba2025c562
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-12-02 00:48:40 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -49,6 +49,31 @@ resource "aws_iam_role_policy" "ec2_ssm_parameters" {
   })
 }
 
+# S3 access for OpenWebUI file storage
+resource "aws_iam_role_policy" "ec2_s3" {
+  name = "${var.project_name}-s3-access"
+  role = aws_iam_role.ec2.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.s3_bucket_name}",
+          "arn:aws:s3:::${var.s3_bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
 # Attach SSM managed policy for Session Manager access
 resource "aws_iam_role_policy_attachment" "ec2_ssm_managed" {
   role       = aws_iam_role.ec2.name
