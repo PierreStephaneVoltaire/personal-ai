72bd7e17670f40a76a39f472a90e5f4358d98165
Pierre-Stephane Voltaire <psvoltaire96@gmail.com>
2025-12-07 04:32:21 -0500

    Update network.tf

--- stacks/ai-platform-base/network.tf
+++ stacks/ai-platform-base/network.tf
@@ -1,19 +1,16 @@
-
-data "kubernetes_service" "nginx_gateway" {
-  metadata {
-    name      = "nginx-gateway-nginx"
-    namespace = "nginx-gateway"
-  }
+data "aws_lb" "nginx_gateway" {
+  name = "nginx-gateway-nlb" 
 }
 
-data "aws_route53_zone" "main" {
-  name = var.domain_name
-}
-
-resource "aws_route53_record" "cluster_wildcard" {
+resource "aws_route53_record" "wildcard" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "*.${var.domain_name}"
   type    = "CNAME"
   ttl     = 300
-  records = [data.kubernetes_service.nginx_gateway.status[0].load_balancer[0].ingress[0].hostname]
+  records = [data.aws_lb.nginx_gateway.dns_name]
+}
+
+
+data "aws_route53_zone" "main" {
+  name = var.domain_name
 }
